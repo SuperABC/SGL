4bbfdeaea6f78a0c9e9989dbcf166715dea6a86f
SG master <1363028533@qq.com>
2017-12-25 00:39:30 +0800

    v3.0.0

--- Sample/Draw/string.c
+++ Sample/Draw/string.c
@@ -8,7 +8,7 @@ void sgLoop() {
 	if (first) {
 		setColor(0, 0, 0);
 		setFontSize(20);
-		setFontName("楷体");
+		setFontName("宋体");
 		putString("SGLv2.2.0开始，SGL支持全部字体输出。", 0, 0);
 		putString("像这样，只要传入字符串，就可以让文本显示在屏幕上了。", 0, 24);
 		first = 0;
