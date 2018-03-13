4e9ada135b5244f14f4429ed18bff986a3eea41f
SG master <1363028533@qq.com>
2018-03-13 17:47:02 +0800

    v3.0.2

--- Sample/Draw/bmp.c
+++ Sample/Draw/bmp.c
@@ -1,7 +1,7 @@
 #include "winsgl.h"
 
 void sgSetup() {
-	initWindow(680, 520, "SGL sample", BIT_MAP);
+	initWindow(0, 0, "SGL sample", BIT_MAP);
 }
 void sgLoop() {
 	static int first = 0;
@@ -11,10 +11,9 @@ void sgLoop() {
 		loadBmp(0, 0, "Source/tower/tower.bmp");
 		getImage(0, 0, 39, 39, buf);
 		putImage(320, 200, buf, COPY_PUT);
+		free(buf->data);
+		free(buf);
 
-		setColor(127, 127, 127);
-		setFontSize(128);
-		putString("Success!", 10, 10);
 		first = 1;
 	}
 }
