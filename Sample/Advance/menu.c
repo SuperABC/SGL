30290b25f6e4ef898c142de980b8004edccebec8
SG master <1363028533@qq.com>
2018-03-13 17:46:38 +0800

    v3.0.2

--- Sample/Advance/menu.c
+++ Sample/Advance/menu.c
@@ -3,27 +3,47 @@
 void callBack() {
 	alertInfo("success.", "Alert", ALERT_BUTTON_OKCANCEL | ALERT_ICON_EXCLAMATION);
 }
+void hide() {
+	hideToTray();
+}
+void close() {
+	exit(0);
+}
+void open() {
+	restoreFromTray();
+}
+
 
 void sgSetup() {
 	initWindow(640, 480, "SGL sample", BIT_MAP);
-	initMenu();
-	int idFile = addMenuList("File", 0);
-	int idEdit = addMenuList("Edit", 0);
-	int idHelp = addMenuList("Help", 0);
-	addMenuItem("Open", idFile, callBack);
+
+	int idMain = initMenu();
+	int idFile = addMenuList("File", idMain);
+	int idEdit = addMenuList("Edit", idMain);
+	int idHelp = addMenuList("Help", idMain);
+	addMenuItem("Open\tCtrl + O", idFile, callBack);
 	addMenuItem("Close", idFile, callBack);
-	addMenuItem("Save", idFile, callBack);
-	addMenuItem("Exit", idFile, callBack);
-	addMenuItem("Copy", idEdit, callBack);
-	addMenuItem("Cut", idEdit, callBack);
-	addMenuItem("Paste", idEdit, callBack);
+	addMenuSeparator(idFile);
+	addMenuItem("Save\tCtrl + S", idFile, callBack);
+	addMenuItem("Hide to tray", idFile, hide);
+	addMenuItem("Exit\tF4", idFile, callBack);
+	addMenuItem("Copy\tCtrl + C", idEdit, callBack);
+	addMenuItem("Cut\tCtrl + X", idEdit, callBack);
+	addMenuItem("Paste\tCtrl + V", idEdit, callBack);
 	addMenuItem("Clear", idEdit, callBack);
 	int idAbout = addMenuList("About", idHelp);
 	addMenuItem("Version", idAbout, callBack);
 	addMenuItem("Author", idAbout, callBack);
 	addMenuItem("Company", idAbout, callBack);
+
+	int idTray = initTrayMenu();
+	addTrayMenuItem("´ò¿ª", idTray, open);
+	addTrayMenuItem("¹Ø±Õ", idTray, close);
 }
 void sgLoop() {
-	setColor(0, 0, 0);
-	clearScreen();
+	static int first = 1;
+	if (first) {
+		first = 0;
+		addTray();
+	}
 }
